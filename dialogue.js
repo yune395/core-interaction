bb17b389117d9dc9c7c5674f072fde33b0e2a062
Emily Yun <yune395@newschool.edu>
2019-02-07 11:37:21 -0500

    edit dialogue

--- dialogue.js
+++ dialogue.js
@@ -1,5 +1,5 @@
 var ol = document.getElementsByTagName('ol');
 
 window.addEventListener("scroll", function() {
-  ol[0].style.opacity = (1 - (document.body.scrollTop / 11000));
+  ol[0].style.opacity = (1 - (document.body.scrollTop / 15000));
 })
